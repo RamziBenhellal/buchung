c70a62db0ff8ab339135aedefa3c309f9412e3f4
moham <moham@BOOK-MQ52UGU0MH>
2023-10-21 20:49:49 +0200

    project creation

---
+++ buchung/src/main/java/com/buchung/BuchungApplication.java
@@ -0,0 +1,13 @@
+package com.buchung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchungApplication.class, args);
+	}
+
+}
---
+++ buchung/src/main/java/com/buchung/model/User.java
@@ -0,0 +1,10 @@
+package com.buchung.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+public class User {
+
+}
