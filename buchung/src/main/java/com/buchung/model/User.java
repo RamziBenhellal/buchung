786560cd3dc727cf17347f536700aea59772c771
moham <moham@BOOK-MQ52UGU0MH>
2023-10-21 21:26:08 +0200

    user model creation

--- buchung/src/main/java/com/buchung/model/User.java
+++ buchung/src/main/java/com/buchung/model/User.java
@@ -1,10 +1,35 @@
 package com.buchung.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
+@Entity(name="User")
+@Table(name="user")
+@NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "userId", updatable = false)
+	private Integer id;
+	
+	@Column(name="vorname")
+	private String vorname;
+	
+	@Column(name="nachname")
+	private String nachname;
+	
+	@Column(name="email")
+	private String email;
+	
+	
 }
